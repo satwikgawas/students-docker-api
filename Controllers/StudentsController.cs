92f7ff87ef8d983ced3467ba5575713a038f7c19
satwik.gawas <satwikgawas1998@gmail.com>
2025-06-18 11:54:32 +0530

    students docker api pushed

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Student_API_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            var students = new[]
+            {
+                new { Id = 1, Name = "Amar", City="Satra" },
+                new { Id = 2, Name = "Akbar", City="Pune" },
+                new { Id = 3, Name = "Mahesh", City="Mumbai" }
+            };
+
+            return Ok(students);
+        }
+    }
+}
