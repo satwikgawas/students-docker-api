3f6fac8aef81d51edd5df46949b89e621a6be0ba
satwik.gawas <satwikgawas1998@gmail.com>
2025-06-26 13:36:48 +0530

    api pushed with docker

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace students_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private static readonly List<object> students_list = new List<object>()
+        {
+            new {Id=1, Name="Amar", Email="amar@gmail.com", Age=27, City="Pune"},
+            new {Id=2, Name="Radha", Email="radha@gmail.com", Age=22, City="Mumbai"},
+            new {Id=3, Name="Sheetal", Email="sheetal@gmail.com", Age=23, City="Nagpur"}
+        };
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            return Ok(students_list);
+
+        }
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM mcr.microsoft.com/dotnet/
+sdk:8.0 AS build
+WORKDIR /app
+
+distinct layers 
+COPY *.csproj ./
+RUN dotnet restore
+
+RUN dotnet publish -c Release -o out
+
+FROM mcr.microsoft.com/dotnet/aspnet:8.0
+WORKDIR /app
+COPY --from=build /app/out ./
+ENTRYPOINT ["dotnet", 
+"students_api.dll"]
